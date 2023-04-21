5813fd5fbff168e40d46a3ce91ba5b25e6be00c6
Muhammad Farhan <farhan4main@gmail.com>
2023-04-21 20:03:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# To run and test the code you need to update 4 places:
+# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
+# 2. Go to your email provider and make it allow less secure apps.
+# 3. Update the SMTP ADDRESS to match your email provider.
+# 4. Update birthdays.csv to contain today's month and day.
 
 from datetime import datetime
 import pandas as pd
@@ -7,17 +12,10 @@ import ssl
 import smtplib
 
 PLACEHOLDER = '[NAME]'
-MY_EMAIL = "from.muhammadfarhan@gmail.com"
-MY_PASSWORD = "wcwplcwwvrqitngr"
+MY_EMAIL = "YOUR EMAIL"
+MY_PASSWORD = "YOUR PASSWORD"
 
-
-##################### Extra Hard Starting Project ######################
-
-# 1. Update the birthdays.csv
-
-# 2. Check if today matches a birthday in the birthdays.csv
-
-birthdays_data = pd.read_csv("./birthdays.csv")
+birthdays_data = pd.read_csv("birthdays.csv")
 birthday_list = birthdays_data.to_dict(orient='records')
 
 today = datetime.now()
@@ -28,48 +26,14 @@ for birthday in birthday_list:
         with open(file_path) as letter:
             letter_content = letter.read()
             letter_content = letter_content.replace(PLACEHOLDER, birthday['name'])
-        em = EmailMessage()
-        em['From'] = MY_EMAIL
-        em['To'] = birthday['email']
-        em['Subject'] = "Happy Birthday!"
+        msg = EmailMessage()
+        msg['From'] = MY_EMAIL
+        msg['To'] = birthday['email']
+        msg['Subject'] = "Happy Birthday!"
         body = letter_content
-        em.set_content(body)
+        msg.set_content(body)
         context = ssl.create_default_context()
-        with smtplib.SMTP_SSL('smtp.gmail.com', context=context) as con:
-            con.login(MY_EMAIL, MY_PASSWORD)
-            con.sendmail(MY_EMAIL, birthday['email'], em.as_string())
-print("All Done!")
-
-
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
-
-# import smtplib
-#
-# my_email = "muhammad_farhanmalik@yahoo.com"
-# password = ""
-#
-# connection = smtplib.SMTP("smtp.mail.yahoo.com")
-# connection.starttls()
-# connection.login(user=my_email, password=password)
-# connection.sendmail(from_addr=my_email, to_addrs="muhammad_farhanmalik@yahoo.com", msg="Subject:Hello\n\nThis is the body of my email.")
-# connection.close()
-# # Note: Use your phone to sign in and 2-Step Verification are both turned off
-# #--------------------------------------------------
-#
-# import datetime as dt
-# datetime = dt.datetime()
-# now = datetime.now()
-# year = now.year
-# day_of_week = now.weekday()
-#
-# date_of_birth = dt.datetime(year=2002, month=5, day=1)
-#
-#
-# with open("quotes.txt" as quotes:
-# 	quote_list = quotes.readlines()
-# 	random_quote = random.choice(quote_list)
-
-# Note: On the Less secure app access
+        with smtplib.SMTP_SSL("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS", context=context) as connection:
+            connection.login(MY_EMAIL, MY_PASSWORD)
+            connection.sendmail(MY_EMAIL, birthday['email'], msg.as_string())
+       
